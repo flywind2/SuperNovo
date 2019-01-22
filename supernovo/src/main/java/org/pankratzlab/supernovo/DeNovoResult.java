0cfc30aa9369e050fea020bb4429c6ba4a59e576
Spencer Kelley <kelle881@umn.edu>
2019-01-22 13:39:33 -0600

    Properly name raw and weighted depth output columns

--- supernovo/src/main/java/org/pankratzlab/supernovo/DeNovoResult.java
+++ supernovo/src/main/java/org/pankratzlab/supernovo/DeNovoResult.java
@@ -20,36 +20,44 @@ public class DeNovoResult {
     OVERLAP_DENOVOS("De_Novo_Variants_Overlapping_Reads", r -> r.getHapResults().getOtherDeNovos()),
     OVERLAP_TRIALLELICS(
         "Triallelic_Variants_Overlapping_Reads", r -> r.getHapResults().getOtherTriallelics()),
-    CHILD_RAW_DEPTH("Depth", r -> r.getChild().getDepth().rawTotalDepth()),
-    CHILD_A1_RAW_DEPTH("Allele_1_Depth", r -> r.getChild().getDepth().allelicRawDepth(r.getA1())),
-    CHILD_A2_RAW_DEPTH("Allele_2_Depth", r -> r.getChild().getDepth().allelicRawDepth(r.getA2())),
-    CHILD_WEIGHTED_DEPTH("Depth", r -> r.getChild().getDepth().weightedTotalDepth()),
+    CHILD_RAW_DEPTH("Raw_Depth", r -> r.getChild().getDepth().rawTotalDepth()),
+    CHILD_A1_RAW_DEPTH(
+        "Allele_1_Raw_Depth", r -> r.getChild().getDepth().allelicRawDepth(r.getA1())),
+    CHILD_A2_RAW_DEPTH(
+        "Allele_2_Raw_Depth", r -> r.getChild().getDepth().allelicRawDepth(r.getA2())),
+    CHILD_WEIGHTED_DEPTH("Weighted_Depth", r -> r.getChild().getDepth().weightedTotalDepth()),
     CHILD_A1_WEIGHTED_DEPTH(
-        "Allele_1_Depth", r -> r.getChild().getDepth().allelicWeightedDepth(r.getA1())),
+        "Allele_1_Weighted_Depth", r -> r.getChild().getDepth().allelicWeightedDepth(r.getA1())),
     CHILD_A2_WEIGHTED_DEPTH(
-        "Allele_2_Depth", r -> r.getChild().getDepth().allelicWeightedDepth(r.getA2())),
+        "Allele_2_Weighted_Depth", r -> r.getChild().getDepth().allelicWeightedDepth(r.getA2())),
     P1_ID("ID", r -> r.getParent1().getId()),
-    P1_RAW_DEPTH("Parent1_Depth", r -> r.getParent1().getDepth().rawTotalDepth()),
+    P1_RAW_DEPTH("Parent1_Raw_Depth", r -> r.getParent1().getDepth().rawTotalDepth()),
     P1_A1_RAW_DEPTH(
-        "Parent1_Allele_1_Depth", r -> r.getParent1().getDepth().allelicRawDepth(r.getA1())),
+        "Parent1_Allele_1_Raw_Depth", r -> r.getParent1().getDepth().allelicRawDepth(r.getA1())),
     P1_A2_RAW_DEPTH(
-        "Parent1_Allele_2_Depth", r -> r.getParent1().getDepth().allelicRawDepth(r.getA2())),
-    P1_WEIGHTED_DEPTH("Parent1_Depth", r -> r.getParent1().getDepth().weightedTotalDepth()),
+        "Parent1_Allele_2_Raw_Depth", r -> r.getParent1().getDepth().allelicRawDepth(r.getA2())),
+    P1_WEIGHTED_DEPTH(
+        "Parent1_Weighted_Depth", r -> r.getParent1().getDepth().weightedTotalDepth()),
     P1_A1_WEIGHTED_DEPTH(
-        "Parent1_Allele_1_Depth", r -> r.getParent1().getDepth().allelicWeightedDepth(r.getA1())),
+        "Parent1_Allele_1_Weighted_Depth",
+        r -> r.getParent1().getDepth().allelicWeightedDepth(r.getA1())),
     P1_A2_WEIGHTED_DEPTH(
-        "Parent1_Allele_2_Depth", r -> r.getParent1().getDepth().allelicWeightedDepth(r.getA2())),
+        "Parent1_Allele_2_Weighted_Depth",
+        r -> r.getParent1().getDepth().allelicWeightedDepth(r.getA2())),
     P2_ID("ID", r -> r.getParent2().getId()),
-    P2_RAW_DEPTH("Parent2_Depth", r -> r.getParent2().getDepth().rawTotalDepth()),
+    P2_RAW_DEPTH("Parent2_Raw_Depth", r -> r.getParent2().getDepth().rawTotalDepth()),
     P2_A1_RAW_DEPTH(
-        "Parent2_Allele_1_Depth", r -> r.getParent2().getDepth().allelicRawDepth(r.getA1())),
+        "Parent2_Allele_1_Raw_Depth", r -> r.getParent2().getDepth().allelicRawDepth(r.getA1())),
     P2_A2_RAW_DEPTH(
-        "Parent2_Allele_2_Depth", r -> r.getParent2().getDepth().allelicRawDepth(r.getA2())),
-    P2_WEIGHTED_DEPTH("Parent2_Depth", r -> r.getParent2().getDepth().weightedTotalDepth()),
+        "Parent2_Allele_2_Raw_Depth", r -> r.getParent2().getDepth().allelicRawDepth(r.getA2())),
+    P2_WEIGHTED_DEPTH(
+        "Parent2_Weighted_Depth", r -> r.getParent2().getDepth().weightedTotalDepth()),
     P2_A1_WEIGHTED_DEPTH(
-        "Parent2_Allele_1_Depth", r -> r.getParent2().getDepth().allelicWeightedDepth(r.getA1())),
+        "Parent2_Allele_1_Weighted_Depth",
+        r -> r.getParent2().getDepth().allelicWeightedDepth(r.getA1())),
     P2_A2_WEIGHTED_DEPTH(
-        "Parent2_Allele_2_Depth", r -> r.getParent2().getDepth().allelicWeightedDepth(r.getA2()));
+        "Parent2_Allele_2_Weighted_Depth",
+        r -> r.getParent2().getDepth().allelicWeightedDepth(r.getA2()));
 
     private static final String DELIM = "\t";
 
