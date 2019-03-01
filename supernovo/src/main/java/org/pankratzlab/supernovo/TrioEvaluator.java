flywind2/SuperNovo

commit c74987641e77567fc87d32ceda3ea5683530143a

Output depths by ref and alt allele

supernovo/src/main/java/org/pankratzlab/supernovo/TrioEvaluator.java
@@ -121,9 +121,9 @@ public class TrioEvaluator {
               new HaplotypeEvaluator(
                       pos, childPile, p1Pileups.getUnchecked(pos), p2Pileups.getUnchecked(pos))
                   .haplotypeConcordance(),
-              generateSample(childID, childPile, childPile),
-              generateSample(parent1ID, p1Pileups.getUnchecked(pos), childPile),
-              generateSample(parent2ID, p2Pileups.getUnchecked(pos), childPile)));
+              generateSample(childID, pos, childPile, childPile),
+              generateSample(parent1ID, pos, p1Pileups.getUnchecked(pos), childPile),
+              generateSample(parent2ID, pos, p2Pileups.getUnchecked(pos), childPile)));
     }
     return Optional.empty();
   }
@@ -155,9 +155,9 @@ public class TrioEvaluator {
         .collect(ImmutableSet.toImmutableSet());
   }
 
-  private static DeNovoResult.Sample generateSample(String id, Pileup pileup, Pileup childPile) {
+  private static DeNovoResult.Sample generateSample(String id, ReferencePosition pos, Pileup pileup, Pileup childPile) {
     return new DeNovoResult.Sample(
-        id, pileup, childPile.getDepth().getA1(), childPile.getDepth().getA2());
+        id, pileup, pos, childPile.getDepth().getA1(), childPile.getDepth().getA2());
   }
 
   public static boolean looksDenovo(Pileup childPileup, Pileup p1Pileup, Pileup p2Pileup) {

supernovo/src/main/java/org/pankratzlab/supernovo/output/DeNovoResult.java
@@ -16,6 +16,8 @@ public class DeNovoResult implements OutputFields {
 
     public final String id;
     public final int rawDepth;
+    public final int refRawDepth;
+    public final Optional<Integer> altRawDepth;
     public final int a1RawDepth;
     public final int a2RawDepth;
     public final int a1ClippedReads;
@@ -23,6 +25,8 @@ public class DeNovoResult implements OutputFields {
     public final int a1UnmappedMateReads;
     public final int a2UnmappedMateReads;
     public final double weightedDepth;
+    public final double refWeightedDepth;
+    public final Optional<Double> altWeightedDepth;
     public final double a1WeightedDepth;
     public final double a2WeightedDepth;
 
@@ -31,14 +35,24 @@ public class DeNovoResult implements OutputFields {
     /**
      * @param id
      * @param pileup
+     * @param pos TODO
      */
-    public Sample(String id, Pileup pileup, Optional<PileAllele> a1, Optional<PileAllele> a2) {
+    public Sample(
+        String id,
+        Pileup pileup,
+        ReferencePosition pos,
+        Optional<PileAllele> a1,
+        Optional<PileAllele> a2) {
       super();
       this.pileup = pileup;
       Depth depth = pileup.getDepth();
+      PileAllele ref = pos.getRefAllele();
+      Optional<PileAllele> alt = pos.getAltAllele();
 
       this.id = id;
       rawDepth = depth.rawTotalDepth();
+      refRawDepth = depth.allelicRawDepth(ref);
+      altRawDepth = alt.map(depth::allelicRawDepth);
       a1RawDepth = a1.map(depth::allelicRawDepth).orElse(0);
       a2RawDepth = a2.map(depth::allelicRawDepth).orElse(0);
       a1ClippedReads = a1.map(pileup.getClippedReadCounts()::count).orElse(0);
@@ -46,6 +60,8 @@ public class DeNovoResult implements OutputFields {
       a1UnmappedMateReads = a1.map(pileup.getUnmappedMateCounts()::count).orElse(0);
       a2UnmappedMateReads = a2.map(pileup.getUnmappedMateCounts()::count).orElse(0);
       weightedDepth = depth.weightedTotalDepth();
+      refWeightedDepth = depth.allelicWeightedDepth(ref);
+      altWeightedDepth = alt.map(depth::allelicWeightedDepth);
       a1WeightedDepth = a1.map(depth::allelicWeightedDepth).orElse(0.0);
       a2WeightedDepth = a2.map(depth::allelicWeightedDepth).orElse(0.0);
     }

commit 00141c40e110c853d63ce5c0237bca84d4b3600f

Fix formatting issues introduced by refactoring
Introduced with c74987641e77567fc87d32ceda3ea5683530143a

supernovo/src/main/java/org/pankratzlab/supernovo/TrioEvaluator.java
@@ -155,7 +155,8 @@ public class TrioEvaluator {
         .collect(ImmutableSet.toImmutableSet());
   }
 
-  private static DeNovoResult.Sample generateSample(String id, ReferencePosition pos, Pileup pileup, Pileup childPile) {
+  private static DeNovoResult.Sample generateSample(
+      String id, ReferencePosition pos, Pileup pileup, Pileup childPile) {
     return new DeNovoResult.Sample(
         id, pileup, pos, childPile.getDepth().getA1(), childPile.getDepth().getA2());
   }